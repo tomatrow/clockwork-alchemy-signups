019a3006e884adde6c34c8e5e857501363fe8342
AJ Caldwell <tomatrow@gmail.com>
2023-03-11 21:41:49 -0800

    Be a bit more explicit with client setup errors

--- src/clients.ts
+++ src/clients.ts
@@ -5,10 +5,22 @@ import Airtable from "airtable"
 export let postmarkClient: ServerClient
 
 export function setup() {
-	Airtable.configure({
-		endpointUrl: "https://api.airtable.com",
-		apiKey: env.AIRTABLE_API_KEY
-	})
+	try {
+		Airtable.configure({
+			endpointUrl: "https://api.airtable.com",
+			apiKey: env.AIRTABLE_API_KEY
+		})
+	} catch(cause) {
+		throw new Error("Airtable setup error", {
+			cause
+		})
+	}
 
-	postmarkClient = new ServerClient(env.POSTMARK_SERVER_TOKEN!)
+	try {
+		postmarkClient = new ServerClient(env.POSTMARK_SERVER_TOKEN!)
+	} catch (cause) {
+		throw new Error("Postmark setup error", {
+			cause
+		})
+	}
 }
