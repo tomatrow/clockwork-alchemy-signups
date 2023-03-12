84b88d9e272aad2319a5e8c6dc62fadeffe942fd
AJ Caldwell <tomatrow@gmail.com>
2023-03-11 21:26:51 -0800

    Add error handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
-import type { Handle } from "@sveltejs/kit"
+import type { Handle, HandleServerError } from "@sveltejs/kit"
 import { setup } from "./clients"
+import crypto from 'crypto'
 
 setup()
 
@@ -7,3 +8,15 @@ export const handle = (async ({ event, resolve }) => {
 	const response = await resolve(event)
 	return response
 }) satisfies Handle
+
+
+
+export const handleError = (({ error, event }) => {
+  const errorId = crypto.randomUUID();
+  
+  console.log(JSON.stringify({
+		errorId,
+		error,
+		event
+	}))
+}) satisfies HandleServerError;
