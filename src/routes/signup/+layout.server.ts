efdfe3e8c2df3103122f868a371f22e8535454e3
AJ Caldwell <tomatrow@gmail.com>
2025-10-09 22:01:45 -0700

    Block signup page for now

--- src/routes/signup/+layout.server.ts
+++ src/routes/signup/+layout.server.ts
@@ -1,9 +1,13 @@
+import { mapValues } from "lodash-es"
+import { error } from "@sveltejs/kit"
+import { dev } from "$app/environment"
 import { getCopy, getAssets, getLeaders, getWorkshops, getAttendees } from "$lib/tables"
 import { getNonsensitiveLeader, getNonsensitiveAttendee } from "$lib/utility"
-import { mapValues } from "lodash-es"
 import type { LayoutServerLoad } from "./$types"
 
 export const load = (async () => {
+	if (!dev) throw error(418, "Temporarily disabled")
+
 	console.log("loading signup layout data")
 
 	return {
