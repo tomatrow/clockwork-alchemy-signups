0e4e20aca06bc88832ef4d5cfcf2db70cbdc6e1f
AJ Caldwell <tomatrow@gmail.com>
2025-10-11 20:37:17 -0700

    Open the flood gates

--- src/routes/signup/+layout.server.ts
+++ src/routes/signup/+layout.server.ts
@@ -1,13 +1,9 @@
 import { mapValues } from "lodash-es"
-import { error } from "@sveltejs/kit"
-import { dev } from "$app/environment"
 import { getCopy, getAssets, getLeaders, getWorkshops, getAttendees } from "$lib/tables"
 import { getNonsensitiveLeader, getNonsensitiveAttendee } from "$lib/utility"
 import type { LayoutServerLoad } from "./$types"
 
 export const load = (async () => {
-	if (!dev) error(418, "Temporarily disabled")
-
 	console.log("loading signup layout data")
 
 	return {
